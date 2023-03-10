ac27e07b7512069f64bb2289cfe6723ece824e3b
Matvii Artemenko <100190098+MatveyArtemenko@users.noreply.github.com>
2023-03-10 18:16:44 +0000

    Initial Commit

---
+++ Project18_LayoutAndGeometry/Project18_LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project18_LayoutAndGeometryApp.swift
+//  Project18_LayoutAndGeometry
+//
+//  Created by admin on 10/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Project18_LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
